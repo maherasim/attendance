3f269eb7b0d8770ca8d76fe38e3c2a9cdf0737b7
maherasim <asimriazasim107@gmail.com>
2026-04-21 11:19:56 +0500

    dashabord issue fixd

--- admin.php
+++ admin.php
@@ -185,7 +185,7 @@ function admin_pager_url(array $base, int $p): string
         <div class="fld fld-actions">
           <button class="btn-secondary" type="submit">Apply</button>
           <button class="btn-clear-filters" type="button" id="admin-clear-filters">Clear filters</button>
-          <button class="btn-export" type="submit" formaction="export.php" formmethod="get">📊 Export Excel</button>
+          <button class="btn-export" type="submit" formaction="export.php" formmethod="get">Export </button>
         </div>
       </form>
     </div>
